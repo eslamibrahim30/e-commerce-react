2e2a78985abc4d775fd91cf9543876a42e8cfcf6
eslamibrahim30 <eslamelbhnaswee110@gmail.com>
2026-03-18 15:26:30 +0200

    Added base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	base: "/e-commerce-react/",
 	resolve: {
 		alias: {
 			'@': new URL('./src', import.meta.url).pathname,
